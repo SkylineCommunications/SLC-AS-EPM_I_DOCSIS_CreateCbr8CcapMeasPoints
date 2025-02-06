0ce63c1e80227534388667fcadc3d036d5bdf5d4
Sebastián Cuéllar <118371671+SebCuellar@users.noreply.github.com>
2025-02-06 13:37:32 -0500

    Changes according QA remarks.

--- SLC-AS-EPM_I_DOCSIS_CreateCbr8CcapMeasPoints_1/SLC-AS-EPM_I_DOCSIS_CreateCbr8CcapMeasPoints_1.cs
+++ SLC-AS-EPM_I_DOCSIS_CreateCbr8CcapMeasPoints_1/SLC-AS-EPM_I_DOCSIS_CreateCbr8CcapMeasPoints_1.cs
@@ -4,15 +4,37 @@ namespace SLCASEPMIDOCSISCreateCbr8CcapMeasPoints_1
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Linq;
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Common;
+	using Skyline.DataMiner.Net.Helper;
 
 	/// <summary>
 	/// Represents a DataMiner Automation script.
 	/// </summary>
 	public class Script
 	{
+		public enum Cbr8CcapParams
+		{
+			InterfacesList = 3001,
+
+			UpPortRead = 3000,
+			UpPortWrite = 3100,
+
+			CenterFreqRead = 3008,
+			CenterFreqWrite = 3108,
+
+			FreqSpanRead = 3009,
+			FreqSpanWrite = 3109,
+
+			DestIdxRead = 3024,
+			DestIdxWrite = 3124,
+
+			InitCaptureRead = 3225,
+			InitCaptureWrite = 3325,
+		}
+
 		/// <summary>
 		/// The script entry point.
 		/// </summary>
@@ -53,15 +75,16 @@ namespace SLCASEPMIDOCSISCreateCbr8CcapMeasPoints_1
 		private static void RunSafe(IEngine engine)
 		{
 			var dms = engine.GetDms();
-			var elements = dms.GetElements();
-			foreach (var element in elements)
+			var elements = dms.GetElements().ToList();
+			foreach (var element in elements.Where(x => x.Name.Equals("CISCO CBR-8 CCAP UTSC") && x.Protocol.Version.Equals("Production")))
 			{
-				if (!element.Protocol.Name.Equals("CISCO CBR-8 CCAP UTSC") || !element.Protocol.Version.Equals("Production"))
-				{
+				var portsString = element.GetStandaloneParameter<string>((int)Cbr8CcapParams.InterfacesList).GetValue();
+				if (portsString.IsNullOrEmpty())
+                {
 					continue;
 				}
 
-				var ports = element.GetStandaloneParameter<string>(3001).GetValue().Split(';');
+				var ports = portsString.Split(';');
 				var itemId = 1;
 				var dmaId = Convert.ToString(element.AgentId);
 				var elementId = Convert.ToString(element.Id);
@@ -77,12 +100,12 @@ namespace SLCASEPMIDOCSISCreateCbr8CcapMeasPoints_1
 						itemStringId, // Measurement Point ID (Unique)
 						string.Format("{0};{0};{0};{0};{0}", dmaId), // DMA id of parameter to set
 						string.Format("{0};{0};{0};{0};{0}", elementId), // Element id(s) of parameter(s) to set
-						"3100;3108;3109;3124;3325", // Write parameter id(s) to set
+						$"{(int)Cbr8CcapParams.UpPortWrite};{(int)Cbr8CcapParams.CenterFreqWrite};{(int)Cbr8CcapParams.FreqSpanWrite};{(int)Cbr8CcapParams.DestIdxWrite};{(int)Cbr8CcapParams.InitCaptureWrite}", // Write parameter id(s) to set
 						$"{elementName}_{port}", // Measurement point name
 						"true;false;false;true;false", // For each parameter to set, "true" if it is of string type
 						$"{port};35;60;1;1", // Value(s) to set in parameter(s)
 						"0", // Delay time after set
-						"3000;3008;3009;3024;3225", // Read id(s) associated with the write parameter ids specified above
+						$"{(int)Cbr8CcapParams.UpPortRead};{(int)Cbr8CcapParams.CenterFreqRead};{(int)Cbr8CcapParams.FreqSpanRead};{(int)Cbr8CcapParams.DestIdxRead};{(int)Cbr8CcapParams.InitCaptureRead}", // Read id(s) associated with the write parameter ids specified above
 						";;;;", // Table indexes of the parameters to set
 						"0.000000", // Freq offset
 						"false", // Needs invert freq
@@ -95,7 +118,14 @@ namespace SLCASEPMIDOCSISCreateCbr8CcapMeasPoints_1
 					newMeasPoints.Add(measPoint);
 				}
 
-				element.SpectrumAnalyzer.SetMeasurementPoints(false, newMeasPoints.ToArray());
+				try
+				{
+					element.SpectrumAnalyzer.SetMeasurementPoints(false, newMeasPoints.ToArray());
+				}
+				catch (Exception e)
+				{
+					engine.Log($"An error has occurred while setting Measurement Points in the {element.Name} element. Error: {e}");
+				}
 			}
 		}
 	}
